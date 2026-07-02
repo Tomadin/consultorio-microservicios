a0a215925d33f0de81f996c2dd88f5d1d7bf0b87
Brian Tomadin <tomadinbrian@gmail.com>
2026-07-02 17:34:18 -0300

    feat(pacientes): add Paciente entity

---
+++ pacientes-service/src/main/java/com/tomadin/pacientes/entities/Paciente.java
@@ -0,0 +1,32 @@
+package com.tomadin.pacientes.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_paciente")
+    private Long idPaciente;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private LocalDate fechaNac;
+    private String telefono;
+
+    public Paciente() {
+    }
+
+    public Paciente(String nombre, String apellido, String dni, LocalDate fechaNac, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.fechaNac = fechaNac;
+        this.telefono = telefono;
+    }
+}
